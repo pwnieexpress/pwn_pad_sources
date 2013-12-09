a61777a4294edf08b6cb363df7e4fdd206c9765d
Awk <awk@pwnieexpress.com>
2013-12-09 16:08:53 -0500

    removed sslstrip folder - sslstrip logs to passwords dir

--- scripts/update_pwnpad.sh
+++ scripts/update_pwnpad.sh
@@ -1,5 +1,26 @@
 #/bin/bash
-cd /opt/pwnix/chef/
-./update.sh stable sensor pwnpad
 
+clear
+echo "    Warning, this update will overwrite any modifed config files !"
+echo
+echo "    Do you want to continue?"
+echo 
+echo "1. Yes"
+echo "2. No"
+echo
+read -p "Choice: " choice
+
+
+if [ $choice -eq 1 ]
+then
+
+/opt/pwnix/chef/update.sh 
+
+echo
+echo 
+echo "    Congradulations your Pad had been updated!"
+
+else
+  exit
+fi
 
