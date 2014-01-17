85632308e421fff77b2d405684bf5dcd785ec741
Gabe Koss <gabe@gabekoss.com>
2014-01-17 10:22:02 -0500

    update airodump.sh

--- scripts/airodump.sh
+++ scripts/airodump.sh
@@ -7,39 +7,42 @@ trap f_cleanup KILL
 
 cd /opt/pwnix/captures/wireless/
 
-f_logornot(){
-
+f_capture_dialogue(){
 	clear
 	echo
-	echo "Would you like to save an airodump capture?"
+	echo "Would you like to save an Airodump capture?"
 	echo
 	echo "Captures saved to /opt/pwnix/captures/wireless/"
 	echo
 	echo "1. Yes"
 	echo "2. No "
 	echo
-
-	read -p "Choice: " logchoice
+  f_validate
 }
 
-f_run(){
-
-	if [ $logchoice -eq 1 ]
-	then
-	airmon-ng start wlan1
-	airodump-ng -w airodump mon0
-	else
-
-	airmon-ng start wlan1
-	airodump-ng mon0
+f_validate(){
+	read -p "Choice: " logchoice
+  if [ $logchoice = 1 -o $logchoice = 2 ]; then
+    f_run $logchoice
+  else
+    echo 'Please enter 1 for yes or 2 for no.'
+    f_validate
 	fi
 }
 
-f_cleanup(){
-airmon-ng stop mon0 
-ifconfig wlan1 down
+f_run(){
+  if [ $1 -eq 1 ]; then
+    airmon-ng start wlan1
+    airodump-ng -w airodump mon0
+  elif [ $1 -eq 2 ]; then
+    airmon-ng start wlan1
+    airodump-ng mon0
+  fi
 }
-f_logornot
-f_run
 
+f_cleanup(){
+  airmon-ng stop mon0
+  ifconfig wlan1 down
+}
 
+f_capture_dialogue
