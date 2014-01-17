99b3078f70e3638d6ec0b48db5b8f19139873c14
Gabe Koss <gabe@gabekoss.com>
2014-01-17 15:01:10 -0500

    Update PwnPAD script

--- scripts/updatepwnpad.sh
+++ scripts/updatepwnpad.sh
@@ -1,26 +1,40 @@
 #/bin/bash
 
-clear
-echo "    Warning, this update will overwrite any modifed config files !"
-echo
-echo "    Do you want to continue?"
-echo
-echo "1. Yes"
-echo "2. No"
-echo
-read -p "Choice: " choice
+f_banner(){
+  clear
+  echo "Pwn Pad Update"
+  echo "Warning, this update will overwrite any modifed config files !"
+  echo
+  echo "Do you want to continue?"
+  echo
+  echo "1. Yes"
+  echo "2. No"
+  echo
+}
 
+f_one_or_two(){
+  read -p "Choice (1 or 2): " input
+  case $input in
+    [1-2]*) echo $input ;;
+    *)
+      f_one_or_two
+      ;;
+  esac
+}
 
-if [ $choice -eq 1 ]
-then
+f_confirm_and_do_update(){
+  if [ $(f_one_or_two) -eq 1 ]; then
+    /opt/pwnix/chef/update.sh
+    echo
+    echo "[+] Congratulations your Pad has been updated!"
+  else
+    echo "[-] Update cancelled."
+    echo "[-] Exiting."
+    exit 1
+  fi
+}
 
-/opt/pwnix/chef/update.sh
+f_banner
+f_confirm_and_do_update
 
-echo
-echo
-echo "    Congratulations your Pad has been updated!"
-
-else
-  exit
-fi
 
