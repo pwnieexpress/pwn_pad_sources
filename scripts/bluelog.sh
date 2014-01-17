832d25c2e57f0b94c22f5104c025382099e2a98c
Gabe Koss <gabe@gabekoss.com>
2014-01-17 18:05:28 -0500

    enhance

--- scripts/bluelog.sh
+++ scripts/bluelog.sh
@@ -6,7 +6,7 @@ hciconfig hci0 up
 cd /opt/pwnix/captures/bluetooth/
 
 echo
-echo "Bluetooth Device Scan log saved in /opt/pwnix/captures/bluetooth/"
+echo "Bluelog Scan log will be saved in /opt/pwnix/captures/bluetooth/"
 echo
 
 bluelog -vtnc -i hci0
