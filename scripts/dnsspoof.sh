724b0fd904794e4457466b3b835955d8c78f7a9b
Gabe Koss <gabe@gabekoss.com>
2014-01-17 22:05:29 -0500

    banner of dnsspoof cleanup

--- scripts/dnsspoof.sh
+++ scripts/dnsspoof.sh
@@ -6,19 +6,20 @@
 
 f_banner(){
   clear
-  echo "*CURRENTLY ONLY WORKS WHEN EVILAP IS RUNNING*"
   echo
   echo "EvilAP Ettercap-NG 0.8.0 DNS Spoofing script"
   echo
-  echo "NOTE: All DNS requests from wireless clients connect to EvilAP "
+  echo "Warning: Currently only works when evilap is running!"
+  echo
+  echo "Note: All DNS requests from wireless clients connect to EvilAP "
   echo "will be redirected to IP of EvilAP (192.168.7.1)"
   echo
   echo "Use with Social Engineering Toolkit (site cloner use 192.168.7.1)"
 }
 
 f_run(){
+  f_banner
   ettercap -i at0 -T -q -P dns_spoof
 }
 
-f_banner
 f_run
