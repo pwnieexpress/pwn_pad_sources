78393b1ac734cce0c8d0731b577ff6a21f568c81
Gabe Koss <gabe@gabekoss.com>
2014-01-20 15:24:22 -0500

    logwiper cleanup pass #1

--- scripts/logwiper.sh
+++ scripts/logwiper.sh
@@ -26,112 +26,92 @@ MISC_FILES=(
   "/opt/pwnix/wireless/wifite/hs/"
 )
 
+f_one_or_two(){
+  read -p "Choice (1 or 2): " input
+  case $input in
+    [1-2]*) echo $input ;;
+    *)
+      f_one_or_two
+      ;;
+  esac
+}
+
 set_choosewisely(){
-  echo '
-  This script will remove ALL LOGS and CAPTURES are you sure you want to continue?
-
-  1. Yes
-  2. No
-
-  '
-  read -p "Choice: " choosewisely
+  echo "[+] This script will remove ALL LOGS and CAPTURES are you sure you want to continue?"
+  echo
+  echo " 1. Yes"
+  echo " 2. No"
+  choosewisely=$(f_one_or_two)
 }
 
 set_clearhistory(){
-  echo '
-  Would you like to remove Bash history as well?
-
-  1. Yes
-  2. No
-
-  '
-  read -p "Choice: " clearhistory
+  echo "[+] Would you like to remove Bash history as well?"
+  echo
+  echo " 1. Yes"
+  echo " 2. No"
+  clearhistory=$(f_one_or_two)
 }
 
 clear_capture_files(){
-  echo 'Removing logs and captures from /opt/pwnix/captures/
-
-  '
-  for file in "${CAPTURE_FILES[@]}"
-  do
+  echo '[+] Removing logs and captures from /opt/pwnix/captures/'
+  for file in "${CAPTURE_FILES[@]}"; do
     echo "  Removing $file"
     wipe -f -i -r $file
   done
 }
 
 clear_misc_files(){
-  echo 'Removing miscelaneous caps and logs / handshakes from other folders
-
-  '
-  for file in "${MISC_FILES[@]}"
-  do
+  echo '[+] Removing miscelaneous caps and logs / handshakes from other folders'
+  for file in "${MISC_FILES[@]}"; do
     echo "  Removing $file"
     wipe -f -i -r $file
   done
 }
 
 clear_tmp_files(){
-  echo 'Removing all files from /tmp/
-
-  '
+  echo '[+] Removing all files from /tmp/'
   wipe -f -i -r /tmp/*
 }
 
 clear_all_files(){
   clear_capture_files
-# clear_misc_files
+  # TODO: should this be removed?
+  # clear_misc_files
   clear_tmp_files
 }
 
 clear_bash_history(){
-                echo '
-      Clearing bash history...
-    '
-               rm -r /root/.bash_history
-               rm -r /home/pwnie/.bash_history
+  echo '[+] Clearing bash history...'
+  rm -r /root/.bash_history
+  rm -r /home/pwnie/.bash_history
 }
 
-initialize(){
+f_initialize(){
   clear
   set_choosewisely
   set_clearhistory
 
-  if [ $choosewisely -eq 1 ]
-   then
+  if [ $choosewisely -eq 1 ]; then
     clear
-
     clear_all_files
 
-    if [ $clearhistory -eq 1 ]
-     then    
+    if [ $clearhistory -eq 1 ]; then
       clear_bash_history
       clear
-      echo "
-  
-        Congratulations all your logs, captures, and bash history have been cleared!
-  
-   Unless of course you forgot about something else this script didn't know about...
-       "
-
-     else
-
+      echo "[+] Congratulations all your logs, captures, and bash history have been cleared!"
+      echo "[+] Unless of course you forgot about something else this script didn't know about..."
+    else
       clear
-      echo "
-                      Not clearing bash history today
-                
-            Congratulations all your logs and captures have been cleared!
-                
-    Unless of course you forgot about something else this script didn't know about...
-               "
-     fi
+      echo "[-] Skipping clearing bash history today"
+      echo "[+] Congratulations all your logs and captures have been cleared!"
+      echo "[+] Unless of course you forgot about something else this script didn't know about..."
+    fi
 
   else
     clear
-    echo "
-     Your logs, captures, and bash history have been left alone.
-     Have a nice day ^_^
-    "
+    echo "[+] Your logs, captures, and bash history have been left alone."
+    echo "[+] Have a nice day ^_^"
   fi
 }
 
-initialize
+f_initialize
