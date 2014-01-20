59d0cbba45417b06cb618517c2c3c95c172e6859
Gabe Koss <gabe@gabekoss.com>
2014-01-20 17:32:37 -0500

    ubertooth tweak

--- scripts/ubertooth.sh
+++ scripts/ubertooth.sh
@@ -14,17 +14,18 @@ f_select(){
   echo "2) ubertooth-dump"
   echo "3) ubertooth-btle"
   read -p "Choice: " selection
-}
 
-f_run(){
-  f_banner
-  f_select
   case $selection in
     1) ubertooth-lap ;;
     2) ubertooth-dump ;;
     3) ubertooth-btle -f ;;
     *) f_select ;;
   esac
+}
+
+f_run(){
+  f_banner
+  f_select
 }
 
 f_run
