932a03e1781d1ea5a207c67bbfb1ad86aa01ff4a
Awk <awk@pwnieexpress.com>
2014-02-04 14:19:05 -0500

    added device identificaiton logic to scripts needing different variables set corresponding to appropriate GSM adapter

--- scripts/sslstrip.sh
+++ scripts/sslstrip.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 #SSLstripp script for sniffing on available interfaces
 
+
+##################################################
+f_identify_device(){
+  # Function to determine whether current device is new pad or old pad
+  # Checking to see if this is the old pad or the new pad:
+  cat /proc/cpuinfo |grep grouper &> /dev/null
+  pad_old_or_new=`echo $?`
+  
+  # If pad_old_or_new = 1 then current device is New Pad
+  if [ $pad_old_or_new -eq 1 ]; then
+
+    # New Pad's GSM interface is rmnet_usb0
+    gsm_int="rmnet_usb0"
+    else
+    # Old Pad's GSM interface is rmnet0
+    gsm_int="rmnet0"
+  fi
+}
+
 ##################################################
 f_interface(){
   clear
@@ -11,7 +30,7 @@ f_interface(){
   echo "3. wlan1  (USB TPlink Atheros)"
   echo "4. mon0  (monitor mode interface)"
   echo "5. at0  (Use with EvilAP)"
-  echo "6. rmnet_usb0 (Internal 3G GSM)"
+  echo "6. $gsm_int (Internal 3G GSM)"
   echo
 
   read -p "Choice: " interfacechoice
@@ -22,7 +41,7 @@ f_interface(){
     3) interface=wlan1 ;;
     4) interface=mon0 ;;
     5) interface=at0 ;;
-    6) interface=rmnet_usb0 ;;
+    6) interface=$gsm_int ;;
     *) f_interface ;;
 esac
 }
@@ -54,5 +73,6 @@ f_run(){
   tail -f $logfile
 }
 
+f_identify_device
 f_run
 
