1d25cc4029cf553182b1621ddc238da28464c711
Gabe Koss <gabe@gabekoss.com>
2014-02-11 11:22:27 -0500

    fix typo

--- scripts/updatepwnpad.sh
+++ scripts/updatepwnpad.sh
@@ -32,7 +32,7 @@ f_confirm_and_do_update(){
     /opt/pwnix/chef/update.sh
     echo
     echo "[+] Congratulations your PwnPad has been updated!"
-    echo "[!] Please update this devices for the update to take effect."
+    echo "[!] Please reboot this devices for the update to take effect."
     echo "[!] Note: if an icon dissapears from your desktop it means that the app has been updated. Please re-add these apps from the main Android app menu."
   else
     echo "[-] Update cancelled."
