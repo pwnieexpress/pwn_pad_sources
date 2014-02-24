735159499944678d64542d0b7410a274d65f1be3
Awk <awk@pwnieexpress.com>
2014-02-23 21:31:17 -0500

    Added script logic to kismet.sh to check processes for bluenmea, if running start gpsd in the background so when Kismet launches so does gpsd

--- scripts/kismet.sh
+++ scripts/kismet.sh
@@ -9,9 +9,25 @@
 trap f_endclean INT
 trap f_endclean KILL
 
+# Function to check for BlueNMEA and start GPSD if present for GPS logging
+f_gps_check(){
+
+  ps ax |grep bluenmea |grep -v grep &> /dev/null
+  GPS_STATUS=$?
+
+  if [ $GPS_STATUS -eq 0 ]; then
+    gpsd -n -D5 tcp://localhost:4352
+  fi
+}
+
 f_endclean(){
   ifconfig wlan1mon down
   ifconfig wlan1 down
+
+
+  if [ $GPS_STATUS -eq 0 ]; then
+    killall -9 gpsd
+  fi
 }
 
 clear
@@ -24,7 +40,7 @@ wait 3
 
 cd /opt/pwnix/captures/wireless/
 
+f_gps_check
 kismet
-
 f_endclean
 
