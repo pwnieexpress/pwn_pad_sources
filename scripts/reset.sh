853e38dc1bfa0a868f3882dec423456c9a522511
Timothy <tim@pwnieexpress.com>
2014-05-16 15:27:27 -0400

    reformat fix

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -32,29 +32,29 @@ f_getstorage(){
   # Set seek size
   if [ $storagesi -gt 20 ]; then # 32G [~26G free]
     # Set seek size to 20G
-    seeksi=20
+    seeksi=20G
   elif [ $storagesi -gt 8 ]; then #16G [~9G free]
     # Set seek size to 7G
-    seeksi=7
+    seeksi=7G
   else # 8G [~?G free]
     # Set seek size to ?G
-    seeksi=4
+    seeksi=4G
   fi
 }
 
 f_reset(){
 
-  # CANNOT USE [*] FOR SERIAL NUMBER OR BACKUP
+  # Get parameters
   f_getserial
   f_getstorage
 
   # Construst cmd for script
   cat << EOF > /cache/recovery/openrecoveryscript
-restore /data/media/0/TWRP/BACKUPS/'$serialno'/'$backup'
+restore /data/media/0/TWRP/BACKUPS/$serialno/$backup
 print  [SETUP STARTED]
 cmd export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH
 cmd mount -o bind /dev /data/local/kali/dev
-cmd chroot /data/local/kali/ /bin/dd if=/dev/zero of=/kali.img bs=1 count=0 seek='$seeksi'G;chroot /data/local/kali/ /sbin/mkfs.ext4 -F /kali.img
+cmd chroot /data/local/kali/ /bin/dd if=/dev/zero of=/kali.img bs=1 count=0 seek=$seeksi;chroot /data/local/kali/ /sbin/mkfs.ext4 -F /kali.img
 cmd mv /data/local/kali/kali.img /data/local/kali_img/;mkdir /data/local/kali_img/kalitmp;mount -t ext4 /data/local/kali_img/kali.img /data/local/kali_img/kalitmp/;cp -a /data/local/kali/* /data/local/kali_img/kalitmp/;umount /data/local/kali_img/kalitmp/;rm -r /data/local/kali_img/kalitmp
 print  [SETUP COMPLETE]
 print  [FACTORY RESET COMPLETE] 
