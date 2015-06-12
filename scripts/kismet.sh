acd126b5164fbaea768b361a9da1555368c2c4a0
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2015-06-11 21:38:56 -0400

    kismet: detect if pulse is using kismet, and stop/restart it appropriately.  also detect if there is a non-pulse kismet or something bound to port 2501

--- scripts/kismet.sh
+++ scripts/kismet.sh
@@ -34,6 +34,57 @@ f_endclean(){
   fi
 }
 
+f_pulse_suspend(){
+  if [ -e /etc/init.d/pwnix_kismet_server ]; then
+    service pwnix_kismet_server status &> /dev/null
+    if [ $? = 0 ]; then
+      printf "Stopping Pwn Pulse kismet service...\n"
+      service pwnix_kismet_server stop
+      service pwnix_kismet_server status &> /dev/null
+      if [ $? = 0 ]; then
+        printf "Failed to stop kismet, please stop it manually.\n"
+        exit 1
+      else
+        RESTART_KISMET=1
+      fi
+    fi
+  fi
+  if [ -z "${RESTART_KISMET}" ]; then
+    find_kismet
+    check_port
+  fi
+}
+
+f_pulse_restore(){
+  if [ "${RESTART_KISMET}" = 1 ]; then
+    printf "Restarting Pwn Pulse kismet service...\n"
+    service pwnix_kismet_server start
+    sleep 2
+    service pwnix_kismet_server status
+    if [ $? = 0 ]; then
+      printf "Successfully restarted Pwn Pulse kismet service.\n"
+    else
+      printf "Failed to restart Pwn Pulse kismet service.\n"
+    fi
+  fi
+}
+
+find_kismet() {
+  found_kismet=$(pgrep -x kismet_server)
+  if [ -n "$found_kismet" ]; then
+    printf "Kismet is already running on PID $found_kismet.\n"
+    exit 1
+  fi
+}
+
+check_port(){
+  port_2501=$(lsof -Pni 4TCP:2501 | grep :2501 | awk '{print $2}')
+  if [ -n "$port_2501" ]; then
+    printf "Something already bound to port 2501 with PID $port_2501\n"
+    exit 1
+  fi
+}
+
 clear
 echo
 echo  "Kismet captures saved to /opt/pwnix/captures/wireless/"
@@ -43,8 +94,10 @@ wait 3
 
 cd /opt/pwnix/captures/wireless/
 
+f_pulse_suspend
 f_uicheck
 f_gps_check
 kismet
 f_endclean
+f_pulse_restore
 
