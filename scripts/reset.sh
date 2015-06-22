5f524ba1cccc9f7f88fe1e36e5358972ee535bcf
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2015-06-21 20:52:05 -0400

    offer chroot only reset option, break commands onto their own lines so I can read easier

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,6 +3,9 @@
 # Result: Stock Pwn Pad 2013/2014 or Pwn Phone 2014
 clear
 
+#Blank everything by default, but allow to blank only some
+: ${chroot_only:=0}
+
 f_run(){
 
   printf '\n[!] FACTORY RESET in progress. All changes will be lost.\n\n'
@@ -44,13 +47,20 @@ f_reset(){
   f_getstorage
 
   # Construst cmd for script
-  cat << EOF > /cache/recovery/openrecoveryscript
-restore /data/media/0/TWRP/BACKUPS/$serialno/$backup
+  rm -f /cache/recovery/openrecoveryscript
+  [ "$chroot_only" = 0 ] && printf "restore /data/media/0/TWRP/BACKUPS/$serialno/$backup\n" > /cache/recovery/openrecoveryscript
+  cat << EOF >> /cache/recovery/openrecoveryscript
 print  [SETUP STARTED]
 cmd export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH
 cmd mount -o bind /dev /data/local/kali/dev
-cmd chroot /data/local/kali/ /bin/dd if=/dev/zero of=/kali.img bs=1 count=0 seek=$seeksi;chroot /data/local/kali/ /sbin/mkfs.ext4 -F /kali.img
-cmd mv /data/local/kali/kali.img /data/local/kali_img/;mkdir /data/local/kali_img/kalitmp;mount -t ext4 /data/local/kali_img/kali.img /data/local/kali_img/kalitmp/;cp -a /data/local/kali/* /data/local/kali_img/kalitmp/;umount /data/local/kali_img/kalitmp/;rm -r /data/local/kali_img/kalitmp
+cmd chroot /data/local/kali/ /bin/dd if=/dev/zero of=/kali.img bs=1 count=0 seek=$seeksi
+cmd chroot /data/local/kali/ /sbin/mkfs.ext4 -F /kali.img
+cmd mv /data/local/kali/kali.img /data/local/kali_img/
+cmd mkdir /data/local/kali_img/kalitmp
+cmd mount -t ext4 /data/local/kali_img/kali.img /data/local/kali_img/kalitmp/
+cmd cp -a /data/local/kali/* /data/local/kali_img/kalitmp/
+cmd umount /data/local/kali_img/kalitmp/
+cmd rm -r /data/local/kali_img/kalitmp
 print  [SETUP COMPLETE]
 print  [FACTORY RESET COMPLETE]
 EOF
