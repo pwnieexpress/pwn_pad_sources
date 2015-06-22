4d1a08be3f98e3f2cb80c6dfa51ff624dab25ba5
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2015-06-22 11:20:53 -0400

    merge new reset code into place

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,7 +2,7 @@
 # Description: Factory resets Pwn Pad 2013/2014 and Pwn Phone 2014
 # Result: Stock Pwn Pad 2013/2014/3 or Pwn Phone 2014
 #set the prompt to the name of the script
-PS1=${PS1//@\\h/@FACTORY RESET}
+PS1=${PS1//@\\h/@FACTORY_RESET}
 clear
 
 #Blank everything by default, but allow to blank only some
@@ -21,45 +21,37 @@ rm -f /cache/recovery/openrecoveryscript
 [ "$chroot_only" = "0" ] && printf "restore /data/media/0/TWRP/BACKUPS/$serialno/$backup\n" > /cache/recovery/openrecoveryscript
 cat << EOF >> /cache/recovery/openrecoveryscript
 print  [SETUP STARTED]
-cmd export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH
-cmd mount -o bind /dev /data/local/kali/dev
-cmd chroot /data/local/kali/ /bin/dd if=/dev/zero of=/kali.img bs=1 count=0 seek=4G
-cmd chroot /data/local/kali/ /sbin/mkfs.ext4 -F /kali.img
-cmd mv /data/local/kali/kali.img /data/local/kali_img/
-cmd mkdir /data/local/kali_img/kalitmp
-cmd mount -t ext4 /data/local/kali_img/kali.img /data/local/kali_img/kalitmp/
-cmd cp -a /data/local/kali/* /data/local/kali_img/kalitmp/
-cmd umount /data/local/kali_img/kalitmp/
-cmd rm -r /data/local/kali_img/kalitmp
+cmd export PATH=/system/xbin:/system/bin:$PATH
+EOF
+if [ -f /data/local/kali_img/kali.img ]; then
+  rm -f /data/local/kali_img/kali.img
+fi
+if [ ! -f /data/local/kali_img/stockchroot.img ]; then
+  #we do not have stockchroot.img, that means we are migrating from v0 to v1
+  cat << EOF >> /cache/recovery/openrecoveryscript
+  busybox dd if=/dev/zero of=/stockchroot.img bs=1 count=0 seek=4G
+  busybox /sbin/mkfs.ext2 -F /data/local/kali_img/stockchroot.img
+  mkdir /data/local/kali_img/kalitmp
+  busybox mount -t ext2 /data/local/kali_img/stockchroot.img /data/local/kali_img/kalitmp/
+  cp -a /data/local/kali/* /data/local/kali_img/kalitmp/
+  busybox umount /data/local/kali_img/kalitmp/
+  rm -r /data/local/kali_img/kalitmp
+EOF
+else
+  #we have stockchroot.img, that means we kill /data/local/kali and unpack there
+  cat << EOF >> /cache/recovery/openrecoveryscript
+  rm -r /data/local/kali/*
+  mkdir /data/local/kali_img/kalitmp
+  busybox mount -t ext2 /data/local/kali_img/stockchroot.img /data/local/kali_img/kalitmp
+  cp -a /data/local/kali_img/kalitmp/* /data/local/kali
+  busybox umount /data/local/kali_img/kalitmp
+  rm -r /data/local/kali_img/kalitmp
+EOF
+fi
+cat << EOF >> /cache/recovery/openrecoveryscript
 print  [SETUP COMPLETE]
 print  [FACTORY RESET COMPLETE]
 EOF
 
 # Reboot into recovery; run script
 reboot recovery
-
-
-#posix
-#if [ -f /data/local/kali_img/kali.img ]; then
-#  rm -f /data/local/kali_img/kali.img
-#fi
-#
-#if [ ! -f /data/local/kali_img/stockchroot.img ]; then
-#  #we do not have stockchroot.img, that means we are migrating from v0 to v1
-#  busybox dd if=/dev/zero of=/stockchroot.img bs=1 count=0 seek=4G
-#  busybox /sbin/mkfs.ext2 -F /data/local/kali_img/stockchroot.img
-#  mkdir /data/local/kali_img/kalitmp
-#  mount -t ext2 /data/local/kali_img/stockchroot.img /data/local/kali_img/kalitmp/
-#  cp -a /data/local/kali/* /data/local/kali_img/kalitmp/
-#  umount /data/local/kali_img/kalitmp/
-#  rm -r /data/local/kali_img/kalitmp
-#else
-#  #we have stockchroot.img, that means we kill /data/local/kali and unpack there
-#  rm -r /data/local/kali/*
-#  mkdir /data/local/kali_img/kalitmp
-#  mount -t ext2 /data/local/kali_img/stockchroot.img /data/local/kali_img/kalitmp
-#  cp -a /data/local/kali_img/kalitmp/* /data/local/kali
-#  umount /data/local/kali_img/kalitmp
-#  rm -r /data/local/kali_img/kalitmp
-#fi
-
