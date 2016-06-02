d19b27473795a1fdf3aeb563ec5f5cb9afef7eec
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2016-06-01 21:53:28 -0400

    dedup some code, cd to the right dir, drop nopromisc flag when logging (why would that have ever been there?)

--- scripts/tshark.sh
+++ scripts/tshark.sh
@@ -8,30 +8,25 @@ clear
 include_cell=1
 . /opt/pwnix/pwnpad-scripts/px_functions.sh
 
-f_savecap(){
+f_savecap() {
   printf "\nSave packet capture to /opt/pwnix/captures/tshark?\n\n"
   printf "1. Yes\n"
   printf "2. No\n\n"
   read -p "Choice: " saveyesno
 
   case $saveyesno in
-    1) f_yes ;;
-    2) f_no ;;
+    1) flags="-w tshark-$(date +%F-%H%M).cap" ;;
+    2) filename="" ;;
     *) f_savecap ;;
   esac
-}
-
-#########################################
-f_yes(){
-  filename=tshark$(date +%F-%H%M).cap
-  tshark -i $interface -w /opt/pwnix/captures/tshark/$filename -P
-}
-
-#########################################
-f_no(){
-  tshark -i $interface
 }
 
 f_interface
 ip link set $interface up
 f_savecap
+
+if [ ! -d /opt/pwnix/captures/tshark ]; then
+  mkdir -p /opt/pwnix/captures/tshark
+fi
+cd /opt/pwnix/captures/tshark
+tshark -i "${interface}" ${flags}
