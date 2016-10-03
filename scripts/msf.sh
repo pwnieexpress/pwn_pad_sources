a51b0855640be75698d8a22853b8769b029807a3
Dallas Winger <dallas@pwnieexpress.com>
2016-10-03 14:53:02 -0400

    trap and cleanup msfconsole

--- scripts/msf.sh
+++ scripts/msf.sh
@@ -5,6 +5,13 @@ PS1=${PS1//@\\h/@msfconsole}
 clear
 
 printf "\n[!] Starting Metasploit.. This is gonna take a sec..\n"
+f_hangup(){
+  pkill -f /usr/bin/msfconsole
+}
+
+trap f_hangup INT
+trap f_hangup KILL
+trap f_hangup SIGHUP
 
 msfconsole
 
